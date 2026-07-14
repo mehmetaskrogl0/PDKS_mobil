f45847b78c93a4a21fade6dd2cfab245e4367c43
mehmetaskrogl0 <mehmetaskrogl0@gmail.com>
2026-07-14 13:27:29 +0300

    backendi kurdum

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/pdks_db"
+DATABASE_URL = "mysql+pymysql://root:Mehmet042@localhost:3306/pdks_db"
 
 
 engine = create_engine(
